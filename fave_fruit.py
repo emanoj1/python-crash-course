685f8eb7b37de3f1c03055255814eee9f5d44aee
emanoj1 <manojventures@gmail.com>
2024-02-18 17:30:01 +1100

    IF condition Favourite fruits

---
+++ fave_fruit.py
@@ -0,0 +1,14 @@
+favorite_fruits = ['mango','papaya','banana']
+
+if 'mango' in favorite_fruits:
+    print("You love mangoes")
+if 'papaya' in favorite_fruits:
+    print("You love papaya")
+if 'banana' in favorite_fruits:
+    print("You love banana")
+if 'dates' in favorite_fruits:
+    print("You love dates")
+if 'watermelon' in favorite_fruits:
+    print("You love watermelon")
+if 'apple' in favorite_fruits:
+    print("You love apple")
