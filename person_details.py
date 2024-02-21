d6b95ef373a87f717ace0d92ca1e0f4bc47cc0c3
emanoj1 <manojventures@gmail.com>
2024-02-21 21:12:02 +1100

    Storing info in a dictionary

---
+++ person_details.py
@@ -0,0 +1,15 @@
+#Storing info about a person using Dictionary
+
+mannys_info = {
+    'first_name': 'manny',
+    'last_name':'vincent',
+    'age': 45,
+    'city': 'sydney'
+}
+
+print(f"My friend's details are below:")
+print(f"His first name is {mannys_info['first_name']}.")
+print(f"His last name is {mannys_info['last_name']}.")
+print(f"He is {mannys_info['age']} year's old.")
+print(f"And he lives in {mannys_info['city']}.")
+      
