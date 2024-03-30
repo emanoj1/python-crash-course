0b0c1820e94d763a35e905e5dd0fb7c71f18a248
emanoj1 <manojventures@gmail.com>
2024-03-30 13:04:03 +1100

    While loop print odd num

---
+++ counting_loop.py
@@ -0,0 +1,7 @@
+# Print odd numbers
+current_number = 0
+while current_number <=25:
+    current_number = current_number + 1
+    if current_number % 2 == 0:
+        continue
+    print(current_number)
