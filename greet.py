f560f5612b74c1fe653311ccb182c19b9a3ce072
emanoj1 <manojventures@gmail.com>
2024-03-30 11:19:22 +1100

    Learn Input

---
+++ greet.py
@@ -0,0 +1,9 @@
+# Learning the INPUT function
+
+name = input("What is your name? :")
+print(f"Your name is {name.title()}.")
+
+# Using Integer as an input
+fave_number = input("What is your favourite number?:")
+fave_number = int(fave_number)
+print(f"{name.title()}'s favoruite number is {fave_number}!")
