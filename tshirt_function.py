e91ad3233ba58b8546d312d5fce450e11732e814
emanoj1 <manojventures@gmail.com>
2024-03-31 11:38:54 +1100

    Learn basic Function

---
+++ tshirt_function.py
@@ -0,0 +1,6 @@
+def make_shirt(tee_size, tee_text):
+    print("Here are the details of your custom t-shirt")
+    print(f"\tT-shirt size: {tee_size}")
+    print(f"\tThe message on it: {tee_text}")
+
+make_shirt('Medium','Just Do It!')
